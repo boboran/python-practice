34a03bdf96aba410c79c710186b1d35d9e3f9744
Unknown <liuboran0413@gmail.com>
2018-03-04 16:40:47 +0800

    add GuessAge.py

--- GuessAge.py
+++ GuessAge.py
@@ -1,3 +1,10 @@
 """
-Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that
+they will turn 100 years old.
 """
+name = input("Please input your name: ")
+age = int(input("Please input your age: "))
+
+year = 2018+(100-age)
+print(name, "will be 100 in", year)
