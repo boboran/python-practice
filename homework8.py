9fec1b67d0c9fc25de9349df84b1e2f966551dbc
ellinx <ellin.xll@gmail.com>
2020-07-23 20:14:16 -0700

    Update hw8 and add hw9.

--- homework8.py
+++ homework8.py
@@ -1,5 +1,9 @@
-pw = int(input('支付宝支付密码：'))
-if pw >= 0 and pw <= 999999:
-    print('支付数字合法')
-else:
-    print('支付数字不合法，请重新输入')
+pw_str = input('支付宝支付密码：')
+pw_num = int(pw_str)
+while True:
+    print("num=",pw_num,"str=", pw_str)
+    if pw_num >= 0 and pw_num <= 999999 and len(pw_str) == 6:
+        print('支付数字合法')
+    else:
+        print('支付数字不合法，请重新输入')
+    pw_str = input('支付宝支付密码：')
---
+++ homework9.py
@@ -0,0 +1,11 @@
+# user_input = input('请输入字母或数字：')
+# while user_input.isalnum():
+#     print('该字母或数字的ASCII值为：'+str(ord(user_input)))
+#     user_input = input('请输入字母或数字：')
+# print('输入无效')
+
+# i = 1
+# while i <= 20:
+#     print(i*"*")
+#     i = i+1
+
